2a5d8985f0816b74f0829ecc9968d4ec6728c977
Aitor <monreaitorcelay@gmail.com>
2020-12-04 16:35:17 +0000

    Change urdf launch file

---
+++ urdf_for_robot_modelling_ws/devel/_setup_util.py
@@ -0,0 +1,1 @@
+/home/aitor/ros/ROS-TheConstruct-Courses/urdf_for_robot_modelling_ws/devel/.private/catkin_tools_prebuild/_setup_util.py
---
+++ urdf_for_robot_modelling_ws/devel/share/my_mira_description/cmake/my_mira_descriptionConfig-version.cmake
@@ -0,0 +1,1 @@
+/home/aitor/ros/ROS-TheConstruct-Courses/urdf_for_robot_modelling_ws/devel/.private/my_mira_description/share/my_mira_description/cmake/my_mira_descriptionConfig-version.cmake
