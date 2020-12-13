c5c677516596e9dc77367aa32afa45f6286cfec4
Aitor <monreaitorcelay@gmail.com>
2020-12-13 23:52:21 +0000

    Reduce joint speed by modifying PID parameters. Limits don't yet work

--- urdf_for_robot_modelling_ws/src/my_gurdy_description/scripts/walk_gurdy.py
+++ urdf_for_robot_modelling_ws/src/my_gurdy_description/scripts/walk_gurdy.py
@@ -19,6 +19,7 @@ class GurdyWalk(object):
         self._pub_lower_M3 = rospy.Publisher('/gurdy/upperlegM3_lowerlegM3_joint_position_controller/command', Float64, queue_size = 1)
         self.upper_pub_value = Float64()
         self.lower_pub_value = Float64()
+        self.z_joint_pub_value = Float64()
         self.ctrl_c = False
         self.topic_name = topic
         self._sub_vel = rospy.Subscriber(self.topic_name, Twist, self.teleop_cb)
@@ -27,8 +28,15 @@ class GurdyWalk(object):
         forwards_vel = msg.linear.x
         self.move_forwards(forwards_vel)
     
-    #def move_forwards(self):
-        
+    def move_forwards(self):
+        self.move_leg_forward(1)
+        self.move_leg_forward(3)
+        self.lift_leg(2)
+        self.short_r.sleep()
+        self.rotate_backwards_leg(1)
+        self.rotate_backwards_leg(3)
+        self.short_r.sleep()
+        self.drop_leg(2)
 
     def move_robot(self):
         self.stand_still()
@@ -63,22 +71,69 @@ class GurdyWalk(object):
         self._pub_z_joint_M1.publish(z_joint_val)
         self._pub_upper_M1.publish(upper_val)
         self._pub_lower_M1.publish(lower_val)
-    
     def M2_leg_publish(self, z_joint_val, upperleg_val, lowerleg_val):
         self._pub_z_joint_M2.publish(z_joint_val)
         self._pub_upper_M2.publish(upper_val)
         self._pub_lower_M2.publish(lower_val)
-
     def M3_leg_publish(self, z_joint_val, upperleg_val, lowerleg_val):
         self._pub_z_joint_M3.publish(z_joint_val)
         self._pub_upper_M3.publish(upper_val)
         self._pub_lower_M3.publish(lower_val)
 
     def move_leg_forward(self, leg_number):
+        self.lift_leg(leg_number)
+        self.short_r.sleep()
+        self.rotate_forwards_leg(leg_number)
+        self.short_r.sleep()
+        self.drop_leg(leg_number)
+        self.short_r.sleep()
+    
+    def lift_leg(self, leg_number):
+        self.upper_pub_value.data = -0.7
+        self.lower_pub_value.data = -0.9
+        self.vertical_publish(leg_number, self.upper_pub_value, self.lower_pub_value)
+    def drop_leg(self, leg_number):
+        self.upper_pub_value.data = -0.5
+        self.lower_pub_value.data = -0.7
+        self.vertical_publish(leg_number, self.upper_pub_value, self.lower_pub_value)
+
+    def vertical_publish(self, leg_number, upperleg_val, lowerleg_val):
+        if leg_number == 1:
+            self.M1_vert_leg_publish(upperleg_val, lowerleg_val)
+        elif leg_number == 2:
+            self.M2_vert_leg_publish(upperleg_val, lowerleg_val)
+        elif leg_number == 3:
+            self.M3_vert_leg_publish(upperleg_val, lowerleg_val)
+    def M1_vert_leg_publish(self, upperleg_val, lowerleg_val):
+        self._pub_upper_M1.publish(upperleg_val)
+        self._pub_lower_M1.publish(lowerleg_val)
+    def M2_vert_leg_publish(self, upperleg_val, lowerleg_val):
+        self._pub_upper_M2.publish(upperleg_val)
+        self._pub_lower_M2.publish(lowerleg_val)
+    def M3_vert_leg_publish(self, upperleg_val, lowerleg_val):
+        self._pub_upper_M3.publish(upperleg_val)
+        self._pub_lower_M3.publish(lowerleg_val)
+    
+    def rotate_forwards_leg(self, leg_number):
         self.z_joint_pub_value.data = 0.75
-        self.upper_pub_value.data = -1.55
-        self.lower_pub_value.data = -1.57
-        self.joint_publish(leg_number, self.z_joint_pub_value, self.upper_pub_value, self.lower_pub_value)
+        self.rotational_publish(leg_number, self.z_joint_pub_value)
+    def rotate_backwards_leg(self, leg_number):
+        self.z_joint_pub_value.data = -0.75
+        self.rotational_publish(leg_number, self.z_joint_pub_value)
+    
+    def rotational_publish(self, leg_number, z_joint_val):
+        if leg_number == 1:
+            self.M1_rot_leg_publish(z_joint_val)
+        elif leg_number == 2:
+            self.M2_rot_leg_publish(z_joint_val)
+        elif leg_number == 3:
+            self.M3_rot_leg_publish(z_joint_val)
+    def M1_rot_leg_publish(self, z_joint_val):
+        self._pub_z_joint_M1.publish(z_joint_val)
+    def M2_rot_leg_publish(self, z_joint_val):
+        self._pub_z_joint_M2.publish(z_joint_val)
+    def M3_rot_leg_publish(self, z_joint_val):
+        self._pub_z_joint_M3.publish(z_joint_val)
 
     def stand_still(self):
         self.z_joint_pub_value.data = 0
@@ -113,4 +168,4 @@ if __name__ == '__main__':
     rospy.on_shutdown(shutdownhook)
 
     while not ctrl_c:
-        gurdywalk.move_leg_forward(1)
+        gurdywalk.move_forwards()
